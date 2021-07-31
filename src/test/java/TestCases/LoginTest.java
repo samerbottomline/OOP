280a5a39180cfc947774499d29c6e002bd863017
Rahal <Samer.Rahal@bottomline.com>
2021-07-31 10:01:24 +0300

    Added Faker API and Random class for data generation

--- src/test/java/TestCases/LoginTest.java
+++ src/test/java/TestCases/LoginTest.java
@@ -4,10 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import com.github.javafaker.Faker;
+
+import java.util.Random;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import ObjectsRepo.TXLoginPage;
+
 
 public class LoginTest {
 	
@@ -35,5 +40,54 @@ public class LoginTest {
 		
 		//objTXLoginpage.ClickLogin();
 	}
+	
+	@Test(description = "Random generator class usage")
+	public void RandmonDataGeneraor() 
+	{
+		Random r = new Random();
+		String firstName = "FirstName"+r.nextInt();
+		System.out.println(firstName);
+		logger.info("firstName initialized " + getClass().getName());
+		String lastName = "LastName"+r.nextInt();
+		System.out.println(lastName);
+		logger.info("lastName initialized " + getClass().getName());
+		String emailAddress = "EmailAddress"+r.nextInt()+"gmail.com";
+		System.out.println(emailAddress);
+		logger.info("EmailAddress initialized " + getClass().getName());
+
+	}
+	
+	@Test(description="Faker API usage")
+	public void FakerDataGenerator() 
+	{
+		//https://github.com/DiUS/java-faker
+		Faker f = new Faker();
+		
+		String firstName = f.name().fullName();
+		System.out.println(firstName);
+		
+		String Address = f.address().fullAddress();
+		System.out.println(Address);
+		
+		String Country = f.address().country();
+		System.out.println(Country);
+		
+		String emailAddress = f.internet().emailAddress();
+		System.out.println(emailAddress);
+		
+		String myHero = f.superhero().name();
+		System.out.println(myHero);
+		
+		String IBAN = f.finance().iban();
+		System.out.println(IBAN);
+		
+		String BIC = f.finance().bic();
+		System.out.println(BIC);
+		
+		System.out.println(f.currency().code());
+	}
+	
+	
+
 
 }
